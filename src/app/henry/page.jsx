5e33c89156ea9b3bbe80722e182df60ed632fa11
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-04-11 10:36:53 +0200

    anotherone

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,15 +1,25 @@
 async function getHenry(){
     const res = await fetch ("https://nice-dogs.vercel.app/api/dogs?slug=henry");
-    const data = await res.json();
-    return data;
+    return await res.json();
+}
+
+export async function generateMetadata(){
+    const data = await getHenry();
+    const {name, age} = data;
+
+    return{
+        title: name,
+        description: `${name} is ${age} years old`,
+    }
 }
 
 export default async function HenryPage(){
     const data = await getHenry();
-    console.log(data);
+    const {name} = data;
+
     return (
         <main>
-            <h1>{data.name}</h1>
+            <h1>{name}</h1>
         </main>
     )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Frontpage",
+  description: "Description",
+}
 export default function Home() {
 
   return (
